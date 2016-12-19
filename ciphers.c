f6d0bdeeb211d41ebcbc926fad0aafe97da55236
Alexander Benson <alexander.benson@student.nmt.edu>
2016-12-18 22:37:24 -0700

    fourth

--- ciphers.c
+++ ciphers.c
@@ -4,5 +4,27 @@
 
 int main()
 {
+        char buf[1024];
+        char *string = "allah";
+        int len;
+        FILE *dictionary;
+        dictionary = fopen("dictionary.txt" , "r");
+        char *found;
+        int foundflag;
+        foundflag = 0;
+        len = strlen(string);
+        while(fgets(buf,1024,dictionary)) {
+                found = strtok(buf, "\n");
+                if(strcmp(string, found) == 0) {
+                        printf("Found: %s\n", buf);
+                        foundflag = 1;
+                        break;
+                }
+
+        }
+        if(foundflag == 0){
+                printf("That was not found in the dictionary\n");
+        }
+        fclose(dictionary);
         return 0;
 }
